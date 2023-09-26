faaab701687f8f4467cfb808e2fa86a748879d2e
Asma <asma.gheisari@gmail.com>
2023-09-26 09:26:28 +0330

    Fixed CookieParser error

--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -2,6 +2,6 @@ import path from "path";
 
 export default class GameService {
   public getGameFilePath() {
-    return path.join(__dirname, "../..", "client", "game.html");
+    return path.join(__dirname, "../../..", "client", "game.html");
   }
 }
